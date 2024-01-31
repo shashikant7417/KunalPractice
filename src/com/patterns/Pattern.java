be3b8010adba04a07764f1814358b6f57654db9a
shashikant7417 <shashikantyadav0@gmail.com>
2024-01-31 23:25:36 +0530

    New patterns added

--- src/com/patterns/Pattern.java
+++ src/com/patterns/Pattern.java
@@ -6,7 +6,9 @@ public class Pattern {
 
         int input = 5;
        // pattern(input);
-        pattern1(input);
+       // pattern1(input);
+       // pattern3(input);
+        pattern4(input);
 
     }
 
@@ -27,13 +29,35 @@ public class Pattern {
 
     static void pattern1(int input){
         for (int row = 1; row <= input ; row++) {
-            for (int col = 0; col <input-row+1 ; col++) {
+            for (int col = 1; col <= input-row+1 ; col++) {
                 System.out.print("* ");
             }
             System.out.println();
 
         }
 
+
+    }
+
+    static void pattern3(int input){
+
+        for(int row = 1; row <= input; row++){
+            for(int col = 1; col <= input-row+1; col++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern4(int input){
+
+        for(int row =1 ; row <= input; row++){
+
+            for(int col = 1; col<=row; col++){
+                System.out.print(col);
+            }
+            System.out.println();
+        }
 
     }
 
